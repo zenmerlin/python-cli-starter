caa5dc969aba235143423f8fc2756ab66d35db4d
Jason Boyll <jason.boyll@gmail.com>
2019-09-25 22:35:07 -0400

    Add utility scripts for automating dev tasks

---
+++ scripts.sh
@@ -0,0 +1,37 @@
+# Utility functions used to script dev tasks. Source this file from the current
+# directory to use. Tested with bash and zsh.
+
+ROOT_PROJECT_DIR=$(pwd)
+
+# Create virtual python environment, activate it, and install the python cli app
+# as editable
+env-init() {
+    cd "${ROOT_PROJECT_DIR}" && \
+    virtualenv .env && \
+    source .env/bin/activate && \
+    pip install --editable .
+}
+
+# Install in virtual environment
+env-install() {
+    cd "${ROOT_PROJECT_DIR}"
+    if [[ -z "$VIRTUAL_ENV" ]]; then
+        source .env/bin/activate
+    fi
+    pip install --editable .
+}
+
+# Activate virtual environment
+start-dev() {
+    cd "${ROOT_PROJECT_DIR}" && \
+    source .env/bin/activate
+}
+
+# Deactivate virtual environment
+stop-dev() {
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        deactivate
+    else
+        echo "Virtual environment is not activated. No action taken."
+    fi
+}
